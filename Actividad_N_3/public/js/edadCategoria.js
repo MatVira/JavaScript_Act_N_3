25cffacd5a39d6052ec42786858fbc642e0946be
MatVira <mviracucha14@gmail.com>
2025-05-16 23:50:12 -0500

    first commit: Creacion de carpetas e index para ejecucion

---
+++ Actividad_N_3/public/js/edadCategoria.js
@@ -0,0 +1,18 @@
+//Actividad 1: IF anidados - Clasificación de edades
+
+// Crea un programa en un archivo .js externo
+// que le pida al usuario (usando prompt) 
+// su edad y muestre en la consola una 
+// categoría según la siguiente tabla:
+
+//Menor de 0: "Edad no válida"
+//0 a 12 años: "Niño"
+//13 a 17 años: "Adolescente"
+//18 a 59 años: "Adulto"
+//60 años o más: "Adulto mayor"
+
+
+//Solicitar la edad25
+
+
+//Funcion if anidado para verificar su edad
