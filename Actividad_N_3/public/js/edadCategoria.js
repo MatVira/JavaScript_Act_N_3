c28e992181dcc3ac5b3be5e11e4d8df348c356a7
MatVira <mviracucha14@gmail.com>
2025-05-17 01:25:58 -0500

    Second commit: ADD Script edadCategoria

--- Actividad_N_3/public/js/edadCategoria.js
+++ Actividad_N_3/public/js/edadCategoria.js
@@ -13,6 +13,19 @@
 
 
 //Solicitar la edad25
+const edad =prompt("Ingresa tu edad");
 
 
 //Funcion if anidado para verificar su edad
+if (edad <= 0) {
+    console.log("Edad no válida");
+} else if ( edad >= 1 && edad <=12 ){
+    console.log("Niño");
+} else if ( edad >= 13 && edad <=17 ){
+    console.log("Adolecente");
+} else if ( edad >= 18 && edad <=59 ){
+    console.log("Adulto");
+} else if (  edad >=60 ){
+    console.log("Adulto mayor");
+}
+
